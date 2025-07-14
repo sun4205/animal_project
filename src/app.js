a1e64db2a14ffc2f20c0c79154d7a1897e7930f6
sun4205 <hanna4205@gmail.com>
2025-07-13 21:13:52 -0400

    count usestate

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function App() {
-    return <div>Animal shows!</div>
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+
+      <div>Number of Animals: {count};</div>
+    </div>
+  );
 }
 
 export default App;
